210d4c878c5fb763f36267c390ed28acb513670d
Jadw1 <jadw1_1@o2.pl>
2020-01-20 21:27:11 +0100

    multiple definition fix

---
+++ playmode.cc
@@ -0,0 +1,43 @@
+#include "playmode.h"
+
+void SequenceMode::play_mode(const std::vector<std::shared_ptr<Playable>> &playables) {
+  for (const std::shared_ptr<Playable>& playable: playables) {
+    playable->play();
+  }
+}
+
+ShuffleMode::ShuffleMode(int seed) : random_engine(seed) {}
+
+void ShuffleMode::play_mode(const std::vector<std::shared_ptr<Playable>> &playables) {   
+  std::vector<size_t> indexes(playables.size());
+  std::iota(indexes.begin(), indexes.end(), 0);
+
+  std::shuffle(indexes.begin(), indexes.end(), random_engine);
+  for(const auto& i: indexes) {
+    playables[i]->play();
+  }
+}
+
+void play_mode(const std::vector<std::shared_ptr<Playable>> &playables) {
+  for (unsigned long i = 1; i < playables.size(); i += 2) {
+    playables[i]->play();
+  }
+
+  for (unsigned long i = 0; i < playables.size(); i += 2) {
+    playables[i]->play();
+  }
+}
+
+std::shared_ptr<ShuffleMode> createShuffleMode(int seed) {
+  return std::make_shared<ShuffleMode>(seed);
+}
+
+std::shared_ptr<OddEvenMode> createOddEvenMode() {
+  static auto oddEvenMode = std::make_shared<OddEvenMode>();
+  return oddEvenMode;
+}
+
+std::shared_ptr<SequenceMode> createSequenceMode() {
+  static auto seqMode = std::make_shared<SequenceMode>();
+  return seqMode;
+}
--- playmode.h
+++ playmode.h
@@ -17,30 +17,15 @@ public:
 class SequenceMode : public PlayMode
 {
 public:
-    void play_mode(const std::vector<std::shared_ptr<Playable>> &playables) override
-    {
-        for (const std::shared_ptr<Playable>& playable: playables)
-        {
-            playable->play();
-        }
-    }
+    void play_mode(const std::vector<std::shared_ptr<Playable>> &playables) override;
 };
 
 class ShuffleMode : public PlayMode
 {
 public:
-    ShuffleMode(int seed) : random_engine(seed) {}
+    ShuffleMode(int seed);
 
-    void play_mode(const std::vector<std::shared_ptr<Playable>> &playables) override
-    {   
-        std::vector<size_t> indexes(playables.size());
-        std::iota(indexes.begin(), indexes.end(), 0);
-
-        std::shuffle(indexes.begin(), indexes.end(), random_engine);
-        for(const auto& i: indexes) {
-            playables[i]->play();
-        }
-    }
+    void play_mode(const std::vector<std::shared_ptr<Playable>> &playables) override;
 
 private:
     std::default_random_engine random_engine;
@@ -49,35 +34,11 @@ private:
 class OddEvenMode : public PlayMode
 {
 public:
-    void play_mode(const std::vector<std::shared_ptr<Playable>> &playables) override
-    {
-        for (unsigned long i = 1; i < playables.size(); i += 2)
-        {
-            playables[i]->play();
-        }
-
-        for (unsigned long i = 0; i < playables.size(); i += 2)
-        {
-            playables[i]->play();
-        }
-    }
+    void play_mode(const std::vector<std::shared_ptr<Playable>> &playables) override;
 };
 
-/*std::shared_ptr<ShuffleMode> createShuffleMode(int seed)
-{
-    return std::make_shared<ShuffleMode>(seed);
-}
-
-std::shared_ptr<OddEvenMode> createOddEvenMode()
-{
-    //to samo co w seq?
-    return std::make_shared<OddEvenMode>();
-}
-
-std::shared_ptr<SequenceMode> createSequenceMode()
-{
-    //statyczna zmienna i ja zwracac?
-    return std::make_shared<SequenceMode>();
-}*/
+std::shared_ptr<ShuffleMode> createShuffleMode(int seed);
+std::shared_ptr<OddEvenMode> createOddEvenMode();
+std::shared_ptr<SequenceMode> createSequenceMode();
 
 #endif
